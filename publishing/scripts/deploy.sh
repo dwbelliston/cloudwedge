f697fc186803b83d064c622edc7101fce1af6c1b
Dustin Belliston <dustinbelliston@gmail.com>
2021-10-24 00:40:37 -0600

    tmp deploy

--- publishing/scripts/deploy.sh
+++ publishing/scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 # For local deploy, when you want to deploy straight to aws
 
-ARTIFACT_BUCKET="cloudwedge-public-artifacts"
+ARTIFACT_BUCKET="cldwdg-west-dev-bucket-artifacts"
 ARTIFACT_BUCKET_PREFIX="local"
 STACK_NAME="cloudwedge"
 CAPABILITIES="CAPABILITY_IAM"
