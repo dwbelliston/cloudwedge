389a60b130ec80742d9a9e879cd4be29db725209
Dustin Belliston <dustinbelliston@gmail.com>
2021-10-26 21:31:00 -0600

    tst wrkflw

--- publishing/scripts/docs-infra.sh
+++ publishing/scripts/docs-infra.sh
@@ -3,14 +3,16 @@
 # Deploy resources for hosting the docs
 
 
-ARTIFACT_BUCKET="cldwdg-west-dev-bucket-artifacts"
-ARTIFACT_BUCKET_PREFIX="local"
-STACK_NAME="cloudwedge-docs"
+CYAN='\033[01;36m'
+NOCOLOR='\033[0m'
+ARTIFACT_BUCKET="cldwdg-west-$TARGET_ENV-bucket-artifacts"
+ARTIFACT_BUCKET_PREFIX="/docs-infra"
 CAPABILITIES="CAPABILITY_IAM"
+STACK_NAME="cloudwedge-docs-infra"
+SOURCE_TEMPLATE="./publishing/docs-infra.yaml"
 
-pwd
-ls
 
-echo $TARGET_ENV
+echo -e "${CYAN}Deploying docs infra to ${TARGET_ENV}${NOCOLOR}";
 
-# sam deploy -t "./publishing/docs-infra.yaml" --stack-name $STACK_NAME --no-confirm-changeset --s3-bucket $ARTIFACT_BUCKET --s3-prefix $ARTIFACT_BUCKET_PREFIX --capabilities $CAPABILITIES --force-upload --no-fail-on-empty-changeset
+
+sam deploy -t $SOURCE_TEMPLATE --stack-name $STACK_NAME --no-confirm-changeset --s3-bucket $ARTIFACT_BUCKET --s3-prefix $ARTIFACT_BUCKET_PREFIX --capabilities $CAPABILITIES --force-upload --no-fail-on-empty-changeset --parameter-overrides Env=$TARGET_ENV
