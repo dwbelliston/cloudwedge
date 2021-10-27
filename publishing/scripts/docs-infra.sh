94fe8fb3f5441d25cfa69f419ec99263c9066fa3
Dustin Belliston <dustinbelliston@gmail.com>
2021-10-27 11:11:19 -0600

    docs infra and sync

--- publishing/scripts/docs-infra.sh
+++ publishing/scripts/docs-infra.sh
@@ -5,14 +5,17 @@
 
 CYAN='\033[01;36m'
 NOCOLOR='\033[0m'
-ARTIFACT_BUCKET="cldwdg-west-$TARGET_ENV-bucket-artifacts"
+ARTIFACT_BUCKET=cldwdg-west-$TARGET_ENV-bucket-artifacts
 ARTIFACT_BUCKET_PREFIX="/docs-infra"
 CAPABILITIES="CAPABILITY_IAM"
 STACK_NAME="cloudwedge-docs-infra"
 SOURCE_TEMPLATE="./publishing/docs-infra.yaml"
+REGION="us-east-1"
 
 
 echo -e "${CYAN}Deploying docs infra to ${TARGET_ENV}${NOCOLOR}";
+echo -e "${CYAN}Deploying docs infra to ${ARTIFACT_BUCKET}${NOCOLOR}";
 
 
-sam deploy -t $SOURCE_TEMPLATE --stack-name $STACK_NAME --no-confirm-changeset --s3-bucket $ARTIFACT_BUCKET --s3-prefix $ARTIFACT_BUCKET_PREFIX --capabilities $CAPABILITIES --force-upload --no-fail-on-empty-changeset --parameter-overrides Env=$TARGET_ENV
+sam deploy -t $SOURCE_TEMPLATE --stack-name $STACK_NAME --no-confirm-changeset --s3-bucket $ARTIFACT_BUCKET --force-upload --no-fail-on-empty-changeset --parameter-overrides Env=$TARGET_ENV --region $REGION
+# --s3-prefix $ARTIFACT_BUCKET_PREFIX
---
+++ publishing/scripts/docs-sync.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Sync docs site to s3
+
+CYAN='\033[01;36m'
+NOCOLOR='\033[0m'
+
+WEBROOT_BUCKET=cw-east-$TARGET_ENV-bucket-web-root
+WEBROOT_LOCALFOLDER="docsbuilt"
+
+
+echo -e "${CYAN}Syncing docs to ${WEBROOT_BUCKET}${NOCOLOR}";
+
+retype build
+aws s3 sync $WEBROOT_LOCALFOLDER s3://$WEBROOT_BUCKET --delete --sse aws:kms
